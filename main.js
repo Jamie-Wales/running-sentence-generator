66f381aaf12804b845781c3e01d09cec55827697
Jamie Wales <jamie.wales.developer@gmail.com>
2020-12-16 17:48:29 +0000

    adding object with sentence structure arrays

---
+++ main.js
@@ -0,0 +1,11 @@
+
+// create object with arrays that will be referenced later when generating random sentence
+
+const sentenceStructure = {
+
+    howFast: ["Today I ran", "Today I walked", "Today I skipped", "Today I crawled"],
+    howFar: ["5km", "10km", "20km", "40km", "50km"],
+    bodyPart: ["my arms", "my legs", "my thighs", "my back"],
+    felt: ["felt great", "felt strong", "felt sore", "felt weak"],
+};
+
