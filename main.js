2204205ca52e90b1751b967cf73ff3f6b851a7b6
Jamie Wales <jamie.wales.developer@gmail.com>
2020-12-16 18:29:31 +0000

    finish off getter then call function

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+//function to generate random number based on length of value in object
+
+const generateRandomNumber = (num) => {
+
+
+    return Math.floor(Math.random() * num);
+
+}
 
 // create object with arrays that will be referenced later when generating random sentence
 
@@ -7,5 +15,14 @@ const sentenceStructure = {
     howFar: ["5km", "10km", "20km", "40km", "50km"],
     bodyPart: ["my arms", "my legs", "my thighs", "my back"],
     felt: ["felt great", "felt strong", "felt sore", "felt weak"],
+    get sentence(){
+        console.log(`${this.howFast[generateRandomNumber(this.howFast.length)]} ${this.howFar[generateRandomNumber(this.howFar.length)]} ${this.bodyPart[generateRandomNumber(this.bodyPart.length)]} ${this.felt[generateRandomNumber(this.felt.length)]}`)
+
+    }
 };
 
+
+
+
+
+sentenceStructure.sentence;
